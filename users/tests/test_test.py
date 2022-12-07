91503e60774246e221a3c9193a0455dbe632188b
yx-xyc <yx2021@nyu.edu>
2022-12-07 14:54:00 -0500

    add tests template

--- users/tests/test_test.py
+++ users/tests/test_test.py
@@ -1,14 +1,17 @@
 import os
 from os.path import dirname, join
 import sys
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from controllers import index
-#import index
 import pymongo
 
 
-def test_github_action():
-   
-    assert 1==1
+class Tests:
+    def test_test1(self):
+        pass
+    
+    def test_test2(self):
+        pass
+
+    def test_test3(self):
+        pass
