5f7da66eef4e2cb0ea59fef7247f51d5ee92a722
yx-xyc <yx2021@nyu.edu>
2022-12-07 15:06:18 -0500

    add tests template

---
+++ admin/tests/test_tests.py
@@ -0,0 +1,17 @@
+import os
+from os.path import dirname, join
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from controllers import index
+import pymongo
+
+
+class Tests:
+    def test_test1(self):
+        pass
+    
+    def test_test2(self):
+        pass
+
+    def test_test3(self):
+        pass
