bf63e48e0edefe7a743f7444da1c97f9afe55a96
cty288 <tc3158@nyu.edu>
2022-12-07 20:47:24 -0500

    login/out + register

--- users/controllers/dashboard.py
+++ users/controllers/dashboard.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
+
 
 dashboard_page = Blueprint( "dashboard", __name__ )
 
-@dashboard_page.route("/")
-def display():
-    return "hello"
+@dashboard_page.route("/dashboard_main")
+def dashboard_main():
+    if not current_user.is_authenticated: 
+        return redirect(url_for('index.home'))
+    return render_template('hello_world.html')
--- users/controllers/index.py
+++ users/controllers/index.py
@@ -1,20 +1,28 @@
-import pymongo
+import os
+from os.path import dirname, join
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from bson.objectid import ObjectId
 from flask import Flask, render_template, request, Blueprint, redirect, url_for, make_response, session, flash
 import datetime
 import os
 import sys
-from dotenv import dotenv_values
-config = dotenv_values(".env")
-index_page = Blueprint( "index_page", __name__ )
+from flask_login import current_user
+from models.database import db
 
+
+
+index_page = Blueprint( "index_page", __name__ )
 app = Flask(__name__)
 app.secret_key = "secret key"
 
-client = pymongo.MongoClient(config["DB_CONNECTION_STRING"])   
-db=client[config["DB_NAME"]]
+
+
 
 @index_page.route('/')
 def home():
-    db.Test.insert_one({"emotion":"test2"})
-    return render_template('/courses/hello_world.html')
+    print("Authentification: " + str(current_user.is_authenticated))
+    if current_user.is_authenticated: 
+        return redirect(url_for('dashboard.dashboard_main'))
+    return render_template('login.html')
