3bf84f4256d51bea2bb7bfa4a878c7f97fb11fcc
okkiris <65980126+okkiris@users.noreply.github.com>
2022-12-09 18:28:28 -0500

    test test2

--- users/tests/test_test.py
+++ users/tests/test_test.py
@@ -1,48 +1,52 @@
 import os
 from os.path import dirname, join, abspath
 import sys
-from models.user import User
-sys.path.append(dirname(dirname(abspath(__file__))))
 
+sys.path.append(dirname(dirname(abspath(__file__))))
+from models.user import User
 from controllers import login
 from controllers import index
 import pymongo
 import ssl
 
+
+
 def test_add_user1():
-    
+    db.Users.remove({ "username": "Danzai" })
     client = pymongo.MongoClient("mongodb+srv://doadmin:fj70nM43lo9I15S2@db-mongodb-nyc1-17689-274bdc70.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-17689")
     #client = pymongo.MongoClient(config["DB_CONNECTION_STRING"]) 
-    db=client["Users_Test"]
+    db=client["Test"]
 
     user_info = login.add_user_to_db("Danzai", "12345", "nyu", "cs", db)
 
 
-    expected_result = User(user_info._id.str,"Danzai", "nyu", "cs")
+    expected_result = User("2333","Danzai", "nyu", "cs")
 
     assert user_info.username == expected_result.username, "failed add user test"
 
+'''
 def test_add_existing_user2():
     
     client = pymongo.MongoClient("mongodb+srv://doadmin:fj70nM43lo9I15S2@db-mongodb-nyc1-17689-274bdc70.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-17689")
     #client = pymongo.MongoClient(config["DB_CONNECTION_STRING"]) 
-    db = client["Users_Test"]
+    db = client["Test"]
 
     user_info = login.add_user_to_db("Danzai", "12345", "nyu", "cs", db)
     
 
-    expected_result = User(user_info._id.str,"Danzai", "nyu", "cs")
+    expected_result = User("00000","Danzai", "nyu", "cs")
 
     assert user_info.username == expected_result.username, "failed add existing user test"
 
-
+'''
 def test_add_user3():
+    db.Users.remove({ "username": "Vincent" })
     
     client = pymongo.MongoClient("mongodb+srv://doadmin:fj70nM43lo9I15S2@db-mongodb-nyc1-17689-274bdc70.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-17689")
     #client = pymongo.MongoClient(config["DB_CONNECTION_STRING"]) 
-    db = client["Users_Test"]
+    db = client["Test"]
 
-    user_info = login.add_user_to_db("Vincent", "00000", "Havard", "econ", db)
+    user_info = login.add_user_to_db("Vincent", "00000", "Harvard", "econ", db)
 
 
     expected_result = User(user_info._id.str,"Vincent", "Harvard", "econ")
@@ -53,7 +57,7 @@ def test_add_user3():
 def test_get_user1():
     client = pymongo.MongoClient("mongodb+srv://doadmin:fj70nM43lo9I15S2@db-mongodb-nyc1-17689-274bdc70.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-17689")
     #client = pymongo.MongoClient(config["DB_CONNECTION_STRING"]) 
-    db=client["Users_Test"]
+    db=client["Test"]
 
     get_user = login.get_user_object_in_db("Vincent", db)
 
@@ -64,11 +68,10 @@ def test_get_user1():
 def test_get_nonexisting_user():
     client = pymongo.MongoClient("mongodb+srv://doadmin:fj70nM43lo9I15S2@db-mongodb-nyc1-17689-274bdc70.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-17689")
     #client = pymongo.MongoClient(config["DB_CONNECTION_STRING"]) 
-    db=client["Users_Test"]
+    db=client["Test"]
 
     get_user = login.get_user_object_in_db("Tim", db)
 
     expected_result = None
 
     assert get_user == expected_result
-
