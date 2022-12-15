5a6b59307a16c85d8e46c83e7ab2f685dac5355c
jiawei-zhang-a <jz4721@nyu.edu>
2022-12-15 01:49:11 -0500

    admin framework built, small modification on front end still needed

---
+++ admin/controllers/database.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request, Blueprint, redirect, url_for, make_response, session, flash
+from dotenv import dotenv_values
+from bson.objectid import ObjectId
+import pymongo
+import requests
+import json
+from os.path import dirname, join, abspath
+import sys
+sys.path.append(dirname(dirname(abspath(__file__))))
+from models.requestCourses import getCourses
+
+
+config = dotenv_values(".env")
+
+database_page = Blueprint("database_page", __name__ )
+
+client = pymongo.MongoClient(config["DB_CONNECTION_STRING"])   
+db=client[config["DB_NAME"]]
+
+url = 'https://schedge.a1liu.com/'
+
+schoolDict = {}
+@database_page.route('/addAll')
+def addAll():
+    schoolAbbrs = db.Schools.find({},{ "_id":0, "schoolFullname":0,"image":0, "subjects":0})
+    for school in schoolAbbrs:
+        schoolAbbr = school["schoolAbbr"]
+        docs = db.Schools.find_one({"schoolAbbr":schoolAbbr}, {"_id":0})
+        subjects = docs["subjects"]
+        for subject in subjects:
+            getCourses(db,schoolAbbr,subject["subjectAbbr"])
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}  
+
+@database_page.route('/displayAlltrue')
+def displayAlltrue():
+    db.courses.update_many({},{"$set":{"display":True}})
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}  
+
+@database_page.route('/displayAllfalse')
+def displayAllfalse():
+    db.courses.update_many({},{"display":False})
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}  
+
+@database_page.route('/update', methods=['POST'])
+def displayUpdate():
+    courseID = request.form.get('courseID')
+    print(courseID)
+    print("hello")
+    type = str(request.form.get('type'))
+    db.Courses.update_one({'_id':ObjectId(courseID)},{'$set':{'display':type}}) #update the display field to True or False
+
+
+@database_page.route('/createSchool')
+def createSchoolsCollection():
+    schoolsAPI = requests.get(url+"schools")
+    schoolsAPI.encoding = 'utf-8'
+    schools  = json.loads(str(schoolsAPI.text))
+
+    subjectsAPI = requests.get(url+"subjects")
+    subjectsAPI.encoding = 'utf-8'
+    subjects  = json.loads(str(subjectsAPI.text))
+
+    i = 10
+    for abbr in schools.keys():
+        key = {"schoolAbbr": abbr}
+        data = {
+            "$set":{"schoolFullname": schools[abbr]["name"], "image":"https://picsum.photos/id/" + str(i)+ "/200/150"}
+        }
+        if abbr != "NT" and abbr != "ND" and abbr != "US" and abbr != "DC":
+            
+            db.Schools.update_one(key, data, upsert=True)
+            i += 1
+            schoolDict[abbr] = schools[abbr]["name"]
+
+    for school in subjects.keys():
+        for abbr in subjects[school].keys():
+            if school in schoolDict.keys():
+                db.Schools.update_one({'schoolAbbr': school}, {'$push': {'subjects': {"subjectAbbr":abbr,"subjectFullname":subjects[school][abbr]["name"],"image":"https://picsum.photos/id/" + str(i)+ "/200/150"}}},upsert=True)
+                i += 1
---
+++ admin/models/requestCourses.py
@@ -0,0 +1,63 @@
+import requests
+import json
+def getCourses(db,schoolAbbr,subjectAbbr):
+
+    schoolFullname = db.Schools.find_one({"schoolAbbr":schoolAbbr},{"_id":0,"schoolFullname":1})["schoolFullname"]
+    subjectFullname = db.Schools.find_one({"schoolAbbr":schoolAbbr},{"_id":0,"subjects":{"$elemMatch":{"subjectAbbr":subjectAbbr}}})["subjects"][0]["subjectFullname"]
+
+    coursesAPI = requests.get("https://schedge.a1liu.com/current/current/"+schoolAbbr + "/" +subjectAbbr + "?full=true" )
+    coursesAPI.encoding = 'utf-8'
+    courses = json.loads(str(coursesAPI.text))
+
+    docs = []
+    for course in courses:
+        course_name = course["name"]
+
+        for section in course["sections"]:
+            doc = {}
+            doc["sectionName"] = section["name"]
+            doc["courseName"] = course_name+" "+section["code"]
+            doc["rawCourseName"] = course["name"]
+            doc["deptCourseId"] = course["deptCourseId"]
+            doc["subjectAbbr"] = course["subjectCode"]["code"]
+            doc["subjectFullname"] = subjectFullname
+            doc["schoolAbbr"] = course["subjectCode"]["school"]
+            doc["schoolFullname"] = schoolFullname
+            doc["registrationNumber"] = section["registrationNumber"]
+            doc["section_code"] = section["code"]
+            doc["type"] = section["type"]
+            doc["status"] = section["status"]
+            doc["location"] = section["location"]
+            doc["grading"] = section["grading"]
+            doc["campus"] = section["campus"]
+            if "description" in course.keys():
+                doc["description"] = course["description"]
+            else:
+                doc["description"] = ""            
+            if "notes" in section.keys():
+                doc["notes"] = section["notes"]
+            else:
+                doc["notes"] = ""
+            if "instructionMode" in section.keys():
+                doc["instructionMode"] = section["instructionMode"]
+            else:
+                doc["instructionMode"] = "TBA"
+            doc["units"] = section["maxUnits"]
+            doc["location"] = section["location"]
+            doc["display"] = False
+            doc["rmpURLs"] = []
+            doc["instructors"] = []
+            doc["overallRating"] = -1
+            db.Courses.update_one({'courseName':doc["courseName"], 'schoolFullname':schoolFullname},{"$set":doc},upsert=True)
+            for professor in section["instructors"]:
+                rmpURL = "https://www.ratemyprofessors.com/"
+                if professor != "Staff":
+                    query = professor.split(' ')
+                    rmpURL = "https://www.ratemyprofessors.com/search/teachers?query=" + query[0] + "%20" + query[1] + "&sid=U2Nob29sLTY3NQ=="
+                db.Courses.update_one({'courseName':doc["courseName"],  'schoolFullname':schoolFullname}, {'$pull': {'rmpURLs':rmpURL, 'instructors': professor}})
+                db.Courses.update_one({'courseName':doc["courseName"],  'schoolFullname':schoolFullname}, {'$push': {'rmpURLs':rmpURL, 'instructors': professor}})
+
+            obj = db.Courses.find_one({'courseName':doc["courseName"],  'schoolFullname':schoolFullname})
+            doc["id"] = str(obj["_id"])
+            docs.append(doc)
+    return docs
