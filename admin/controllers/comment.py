661a8c2278822774e00bd371e23d92e0124b8f68
cty288 <tc3158@nyu.edu>
2022-12-16 22:09:33 -0500

    update

---
+++ admin/controllers/comment.py
@@ -0,0 +1,6 @@
+class Comment():
+    def __init__(self, username:str, comment: str, rating: int):
+        self.username = username
+        self.comment = comment
+        self.rating = rating
+    
